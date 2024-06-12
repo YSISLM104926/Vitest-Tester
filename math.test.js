80e77b6d4b4f9b431101c2cd9f50419aba24ca56
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-12 17:33:49 +0600

    added

---
+++ math.test.js
@@ -0,0 +1,28 @@
+import {it, expect} from 'vitest'
+import {Add} from './math'
+
+
+it('it need to be return the correct sum', ()=> {
+    let numbers = [1,2,3];
+    let expectedResult = numbers.reduce((acc,cur)=> acc+cur);
+    let result = Add(numbers);
+    expect(result).toBe(expectedResult);
+})
+
+
+it('it need to be return NaN', ()=> {
+    // Arrange
+    let numbers = [1,'invalid',3]
+    // Actions
+    let expectedResult = numbers.reduce((acc,cur)=> acc+cur)
+    let result = Add(numbers);
+    // Assertions
+    expect(result).toBeNaN();
+})
+
+it('it need to be return the correct sum', ()=> {
+    let numbers = ['1','2','3'];
+    let expectedResult = numbers.reduce((acc,cur)=> acc+cur);
+    let result = Add(numbers);
+    expect(result).toBe(expectedResult);
+})
--- math.test.ts
+++
@@ -1,9 +0,0 @@
-import {it, expect} from 'vitest'
-import {Add} from './math'
-
-it('it need to be return the correct sum', ()=> {
-    let numbers = [1,2,3];
-    let expectedResult = numbers.reduce((acc,cur)=> acc+cur);
-    let result = Add(numbers);
-    expect(result).toBe(expectedResult);
-})
