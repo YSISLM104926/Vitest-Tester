87e245e1d06061a261626e4ce5ea145858025f91
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-12 17:04:57 +0600

    added

--- math.test.ts
+++ math.test.ts
@@ -2,6 +2,8 @@ import {it, expect} from 'vitest'
 import {Add} from './math'
 
 it('it need to be return the correct sum', ()=> {
-    let result = Add([1,2,3]);
-    expect(result).toBe(6);
+    let numbers = [1,2,3];
+    let expectedResult = numbers.reduce((acc,cur)=> acc+cur);
+    let result = Add(numbers);
+    expect(result).toBe(expectedResult);
 })
